cfd3c6319fb9d1192e8bf0657eb568a76d0d2b17
J-dar Siegfred Rodriguez <siegfred.rodriguez@gmail.com>
2024-11-16 14:09:34 +0000

    initial set

---
+++ src/main/java/dev/sieg/java_test_bed/JavaTestBedApplication.java
@@ -0,0 +1,13 @@
+package dev.sieg.java_test_bed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaTestBedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaTestBedApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/sieg/java_test_bed/JavaTestBedApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sieg.java_test_bed;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTestBedApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
